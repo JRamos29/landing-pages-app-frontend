656161bd04f3531f2ba65a3f005012ac3283b1d2
JRamos29 <malebolge@gmail.com>
2021-10-21 13:44:50 -0300

    Added media query for Heading fonts. Fixed fonts imports.

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,7 +1,7 @@
 import P from 'prop-types';
 import * as Styled from './styles';
 
-export const Heading = ({ children, colorDark = true, as = 'h1', size = 'large', uppercase = false }) => {
+export const Heading = ({ children, colorDark = true, as = 'h1', size = 'huge', uppercase = false }) => {
   return (
     <Styled.Title colorDark={colorDark} as={as} size={size} uppercase={uppercase}>
       {children}
@@ -11,8 +11,8 @@ export const Heading = ({ children, colorDark = true, as = 'h1', size = 'large',
 
 Heading.propTypes = {
   children: P.node.isRequired,
-  colorDark: P.bool.isRequired,
-  as: P.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']).isRequired,
-  size: P.oneOf(['small', 'medium', 'large', 'huge']).isRequired,
-  uppercase: P.bool.isRequired,
+  colorDark: P.bool,
+  as: P.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
+  size: P.oneOf(['small', 'medium', 'large', 'huge']),
+  uppercase: P.bool,
 };
--- src/components/Heading/styles.js
+++ src/components/Heading/styles.js
@@ -15,8 +15,15 @@ const titleSizes = {
   huge: (theme) =>
     css`
       font-size: ${theme.font.sizes.xhuge};
+      ${mediaFont(theme)};
     `,
 };
+
+const mediaFont = (theme) => css`
+  @media ${theme.media.lteMedium} {
+    font-size: ${theme.font.sizes.xlarge};
+  }
+`;
 
 const titleCase = (uppercase) => css`
   text-transform: ${uppercase ? 'uppercase' : 'none'};
