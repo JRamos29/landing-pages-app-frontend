bdc5f8bb2ec94ec05a7b9f1738721d1718d22021
JRamos29 <malebolge@gmail.com>
2021-10-21 00:52:09 -0300

    Additional configuration with Storybook.

--- src/templates/Home/stories.jsx
+++ src/templates/Home/stories.jsx
@@ -3,6 +3,17 @@ import Home from './index';
 export default {
   title: 'Home',
   component: Home,
+  args: {
+    children: 'Empty',
+  },
+  argTypes: {
+    children: { type: 'string' },
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
+  },
 };
 
 export const Template = (args) => <Home {...args} />;
