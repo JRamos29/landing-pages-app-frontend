fc7e407deb102d4260af5434b8c858a1d01829ce
JRamos29 <malebolge@gmail.com>
2021-10-24 18:07:19 -0300

    Adding the components to the Home page.

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from 'react';
-import { Base } from '../Base';
-import { mockBase } from '../Base/mock';
+
 import { mapData } from '../../api/map-data';
+
+import { GridTwoColumns } from '../../components/GridTwoColumns';
+import { GridContent } from '../../components/GridContent';
+import { GridText } from '../../components/GridText';
+import { GridImage } from '../../components/GridImage';
+
+import { Base } from '../Base';
 import { PageNotFound } from '../PageNotFound';
 import { Loading } from '../Loading';
 
@@ -18,6 +24,7 @@ function Home() {
         const pageData = mapData(json);
         setData(pageData[0]);
       } catch (e) {
+        console.log(e);
         setData(undefined);
       }
     };
@@ -33,7 +40,36 @@ function Home() {
     return <Loading />;
   }
 
-  return <Base {...mockBase} />;
+  const { menu, sections, footerHtml, slug } = data;
+  const { links, text, link, srcImg } = menu;
+
+  return (
+    <Base
+      links={links}
+      footerHtml={footerHtml}
+      logoData={{ text, link, srcImg }}>
+      {sections.map((section, index) => {
+        const { component } = section;
+        const key = `${slug}-${index}`;
+
+        if (component === 'section.section-two-columns') {
+          return <GridTwoColumns key={key} {...section} />;
+        }
+
+        if (component === 'section.section-content') {
+          return <GridContent key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-text') {
+          return <GridText key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-image') {
+          return <GridImage key={key} {...section} />;
+        }
+      })}
+    </Base>
+  );
 }
 
 export default Home;
