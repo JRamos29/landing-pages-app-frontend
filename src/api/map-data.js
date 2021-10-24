c9acd396a68867f03eaa5f2c31f78309d0d9b117
JRamos29 <malebolge@gmail.com>
2021-10-23 21:57:57 -0300

    Mapping data from API to GridTwoColuns component. Mapping data from API to GridContent component. Mapping data from API to GridText component. Mapping data from API to GridImage component.

--- src/api/map-data.js
+++ src/api/map-data.js
@@ -1,3 +1,10 @@
+import {
+  mapMenu
+} from './map-menu';
+import {
+  mapSections
+} from './map-sections';
+
 export const mapData = (pagesData = [{}]) => {
   return pagesData.map((data) => {
     const {
@@ -12,8 +19,8 @@ export const mapData = (pagesData = [{}]) => {
       footerHtml,
       slug,
       title,
-      sections,
-      menu,
+      sections: mapSections(sections),
+      menu: mapMenu(menu),
     };
   });
 };
