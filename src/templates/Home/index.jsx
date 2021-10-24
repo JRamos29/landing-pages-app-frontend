d5b5b8360666c059eaf92f682e4674eadfdb577a
JRamos29 <malebolge@gmail.com>
2021-10-24 17:11:51 -0300

    Loading data from API. Implemented PageNotFound template page.

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from 'react';
 import { Base } from '../Base';
 import { mockBase } from '../Base/mock';
+import { mapData } from '../../api/map-data';
+import { PageNotFound } from '../PageNotFound';
 
 function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const load = async () => {
+      try {
+        const data = await fetch(
+          'http://localhosta:1337/pages/?slug=landing-page',
+        );
+        const json = await data.json();
+        const pageData = mapData(json);
+        setData(pageData[0]);
+      } catch (e) {
+        setData(undefined);
+      }
+    };
+
+    load();
+  }, []);
+
+  if (data === undefined) {
+    return <PageNotFound />;
+  }
+
+  if (data && !data.slug) {
+    return <h1>Loading...</h1>;
+  }
+
   return <Base {...mockBase} />;
 }
 
